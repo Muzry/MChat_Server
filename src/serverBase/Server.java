d3d8a96315fd64424abdae8e6fc839617d7deb0b
muzry <muzrry@gmail.com>
2017-06-18 13:01:52 +0800

    增加通讯功能

--- src/serverBase/Server.java
+++ src/serverBase/Server.java
@@ -1,13 +1,46 @@
 package serverBase;
 
-public class Server {
-	public Server()
-	{
-		
-	}
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	public void start() 
+import com.google.gson.Gson;
+
+public class Server {
+	
+	private static ServerSocket serverSocket;
+	
+	public Gson gson = new Gson();
+	
+	public void initServer()
 	{
-		System.out.println("Starting Server");
+		try{
+			serverSocket = new ServerSocket(SocketUrls.PORT);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private void createMessage()
+	{
+		try
+		{
+			System.out.println("等待用户接入: ");
+			Socket socket = serverSocket.accept();
+			
+			System.out.println("用户接入: " + socket.getPort());
+			
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					createMessage();
+					}
+				}).start();;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/serverBase/SocketUrls.java
@@ -0,0 +1,6 @@
+package serverBase;
+
+public class SocketUrls {
+	public final static String IP = "127.0.0.1";
+	public final static int PORT = 8080;
+}
